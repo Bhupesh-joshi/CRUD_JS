f0de42eedbd8ac271cf23777ebe6fef58d788889
Bhupesh Joshi <78612301+Bhupesh-joshi@users.noreply.github.com>
2024-02-20 15:28:48 +0530

    CRUD Operations question 3

---
+++ Simple_Questions/q3.js
@@ -0,0 +1,8 @@
+// Write program, which reads 4 numbers a, b, c and p. Let fx)=ax-+bxte be a function. 
+// The program outputs the value of flp). e.g input 4 3-1 2 output 4(2)+3(2)-1=21.
+
+let a = 4, b = 3, c = -1;
+let p = 2;
+
+let value = a * p * p + b * p + c;
+console.log(value);
