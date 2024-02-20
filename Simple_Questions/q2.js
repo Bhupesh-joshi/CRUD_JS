bb29eef8000700ad111879effb2506f96d10bd76
Bhupesh Joshi <78612301+Bhupesh-joshi@users.noreply.github.com>
2024-02-20 15:16:56 +0530

    CRUD Operations question 2

---
+++ Simple_Questions/q2.js
@@ -0,0 +1,10 @@
+// Write program, which reads 2 numbers and prints the sum of square of the first and 
+// cube of the second. e.g. input 5 3 output 5+3 =52. 
+
+let a = 5;
+let b = 3;
+
+let square = a * a;
+let cube = b * b * b;
+let sum = square + cube;
+console.log(sum);
