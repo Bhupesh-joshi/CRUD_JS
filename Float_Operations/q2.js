b85068c536ce3472d7fa6b63713923edb5cfedf2
Bhupesh Joshi <78612301+Bhupesh-joshi@users.noreply.github.com>
2024-02-22 01:24:17 +0530

    CRUD Float question 2 done

--- Float_Operations/q2.js
+++ Float_Operations/q2.js
@@ -1,2 +1,7 @@
 // Write program, which reads a, b, c and d and finds distance between points (a,b) and 
 // (c,d). input 3, 7, 11, 13 output 10. 
+
+let a = 3, b =7, c = 11, d = 13;
+// let distance = Math.sqrt((c-a)*(c-a)+(d-b)*(d-b));
+let distance = Math.sqrt(Math.pow(c-a, 2)+ Math.pow(d-b, 2));
+console.log(distance);
