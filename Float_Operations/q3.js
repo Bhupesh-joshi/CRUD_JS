fd1386e0ac6ad9d0ecf196704f0fc611393e73d1
Bhupesh Joshi <78612301+Bhupesh-joshi@users.noreply.github.com>
2024-02-22 23:12:13 +0530

    CRUD Float question 3 done

---
+++ Float_Operations/q3.js
@@ -0,0 +1,5 @@
+// Write program, which reads 6 numbers a, b, c, d, e and f. The program outputs the area 
+// of the triangle whose end points are (a,b), (c,d) and (e,f). [Hint: use above two 
+// questions]. Input 7 3 11 3 76output 6.
+
+let a = 7, b = 3, c = 11, d = 3;
